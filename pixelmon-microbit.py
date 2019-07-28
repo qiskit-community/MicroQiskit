300257faf13f11c8db7c4871ea3fe31460182bbd
James Wootton <decodoku@gmail.com>
2019-07-28 14:15:06 +0100

    try single qubit microbit

--- pixelmon-microbit.py
+++ pixelmon-microbit.py
@@ -1,8 +1,5 @@
 from microbit import *
 import random
-
-import random
-import numpy as np
 pi = 3.14159265359
 def cos(t):
     t = t % (2*pi)
@@ -13,17 +10,11 @@ def cos(t):
         c += term
     return c
 def sin(t):
-    t = t % (2*pi)
-    s = t
-    term = t
-    for j in range(2,16):
-        term = -term * t**2/((2*j-1)*(2*j-2))
-        s += term
-    return s
+    return cos(t-pi/2)
 class QuantumCircuit:
     def __init__(self,nq):
         self._nq = nq
-        self.p2s = ['I','X','Y','Z']*(self._nq==2) + ['']*(self._nq==1)
+        self.p2s =['']
         self.expectations = {}
         self.reset()
     def _pauli(self,p1,p2,q):
@@ -36,16 +27,12 @@ class QuantumCircuit:
             for p2 in self.p2s:
                 self.expectations[p1+p2] = 0
         for p1 in ['I','Z']:
-            for p2 in ['I','Z']*(self._nq==2) + ['']*(self._nq==1):
+            for p2 in ['']:
                 self.expectations[p1+p2] = 1
     def x(self,q):
         for p2 in self.p2s:
             for p1 in ['Y','Z']:
                 self.expectations[self._pauli(p1,p2,q)] = -self.expectations[self._pauli(p1,p2,q)]   
-    def z(self,q):
-        for p2 in self.p2s:
-            for p1 in ['X','Z']:
-                self.expectations[self._pauli(p1,p2,q)] = -self.expectations[self._pauli(p1,p2,q)]
     def h(self,q):
         for p2 in self.p2s:
             temp = self.expectations[self._pauli('X',p2,q)]
@@ -60,30 +47,10 @@ class QuantumCircuit:
             old_y = self.expectations[self._pauli('Y',p2,q)]
             self.expectations[self._pauli('X',p2,q)] = c*old_x - s*old_y
             self.expectations[self._pauli('Y',p2,q)] = c*old_y + s*old_x
-    def rx(self,theta,q):
-        self.h(q)
-        self.rz(theta,q)
-        self.h(q)
-    def cz(self,c=0,t=0):
-        if self._nq==2:
-            for pair in [('XI','XZ'),('IX','ZX'),('YI','YZ'),('IY','ZY'),('XX','YY')]:
-                temp = self.expectations[pair[0]]
-                self.expectations[pair[0]] = self.expectations[pair[1]]
-                self.expectations[pair[1]] = temp
-    def cx(self,c,t):
-        self.h(t)
-        self.cx(c,t)
-        self.h(t)
 def execute(qc,shots=1024):
     probs = {}
-    if qc._nq==2:
-        probs['00'] = (1+qc.expectations['IZ']+qc.expectations['ZI']+qc.expectations['ZZ'])/4
-        probs['01'] = (1-qc.expectations['IZ']+qc.expectations['ZI']-qc.expectations['ZZ'])/4
-        probs['10'] = (1+qc.expectations['IZ']-qc.expectations['ZI']+qc.expectations['ZZ'])/4
-        probs['11'] = (1-qc.expectations['IZ']-qc.expectations['ZI']-qc.expectations['ZZ'])/4
-    elif qc._nq==1:
-        probs['0'] = (1+qc.expectations['Z'])/2
-        probs['1'] = (1-qc.expectations['Z'])/2
+    probs['0'] = (1+qc.expectations['Z'])/2
+    probs['1'] = (1-qc.expectations['Z'])/2
     counts = {}
     if shots==0:
         for string in probs:
@@ -106,11 +73,18 @@ rock = 5
 player = 9
 animal = 9
 
+seed = [random.randint(1,7) for _ in range(4)]
+qc = QuantumCircuit(1)
+
 def get_brightness(x,y,X,Y):
     qc.reset()
-    qc.rx(pi**2*seed[0]*(x+X),0)
+    qc.h(0)
+    qc.rz(pi**2*seed[0]*(x+X),0)
+    qc.h(0)
     qc.rz(pi/2,0)
-    qc.rx(pi**2*seed[1]*(y+Y),0)
+    qc.h(0)
+    qc.rz(pi**2*seed[1]*(y+Y),0)
+    qc.h(0)
     qc.rz(-pi/2,0)
     colour  = rock * (qc.expectations['Z']>=0.5) + animal * (qc.expectations['Z']<-0.95)
     return colour
@@ -123,9 +97,8 @@ def teleport(x,y):
         y += random.randint(-2,2)
     return x,y
 
-seed = [random.randint(1,7) for _ in range(4)]
 (x,y) = teleport(0,0)
-
+  
 caught = []
 escaped = []
 while True:
