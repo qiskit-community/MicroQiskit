da31eba8cf98231bee6e3211655c266b4ae7cbfb
James Wootton <decodoku@gmail.com>
2019-07-29 10:37:18 +0100

    reduce size

--- aether.py
+++ aether.py
@@ -1,99 +1,77 @@
 import random
-pi = 3.14159265359
+pi=3.14159265359
 def cos(t):
-    t = t % (2*pi)
-    c = 1
-    term = 1
-    for j in range(1,16):
-        term = -term * t**2/(2*j*(2*j-1))
-        c += term
-    return c
+  t=t%(2*pi)
+  c=1
+  tm=1
+  for j in range(1,16):
+    tm=-tm*t**2/(2*j*(2*j-1))
+    c+=tm
+  return c
 def sin(t):
-    t = t % (2*pi)
-    s = t
-    term = t
-    for j in range(2,16):
-        term = -term * t**2/((2*j-1)*(2*j-2))
-        s += term
-    return s
+  return cos(t-pi/2)
 class QuantumCircuit:
-    def __init__(self,nq):
-        self._nq = nq
-        self.p2s = ['I','X','Y','Z']*(self._nq==2) + ['']*(self._nq==1)
-        self.expectations = {}
-        self.reset()
-    def _pauli(self,p1,p2,q):
-        if q==1:
-            return p1+p2
-        else:
-            return p2+p1
-    def reset (self):
-        for p1 in ['I','X','Y','Z']:
-            for p2 in self.p2s:
-                self.expectations[p1+p2] = 0
-        for p1 in ['I','Z']:
-            for p2 in ['I','Z']*(self._nq==2) + ['']*(self._nq==1):
-                self.expectations[p1+p2] = 1
-    def x(self,q):
-        for p2 in self.p2s:
-            for p1 in ['Y','Z']:
-                self.expectations[self._pauli(p1,p2,q)] = -self.expectations[self._pauli(p1,p2,q)]   
-    def z(self,q):
-        for p2 in self.p2s:
-            for p1 in ['X','Z']:
-                self.expectations[self._pauli(p1,p2,q)] = -self.expectations[self._pauli(p1,p2,q)]
-    def h(self,q):
-        for p2 in self.p2s:
-            temp = self.expectations[self._pauli('X',p2,q)]
-            self.expectations[self._pauli('X',p2,q)] = self.expectations[self._pauli('Z',p2,q)]
-            self.expectations[self._pauli('Z',p2,q)] = temp
-            self.expectations[self._pauli('Y',p2,q)] = -self.expectations[self._pauli('Y',p2,q)]
-    def rz(self,theta,q):
-        c = cos(theta)
-        s = sin(theta)
-        for p2 in self.p2s:
-            old_x = self.expectations[self._pauli('X',p2,q)]
-            old_y = self.expectations[self._pauli('Y',p2,q)]
-            self.expectations[self._pauli('X',p2,q)] = c*old_x - s*old_y
-            self.expectations[self._pauli('Y',p2,q)] = c*old_y + s*old_x
-    def rx(self,theta,q):
-        self.h(q)
-        self.rz(theta,q)
-        self.h(q)
-    def cz(self,c=0,t=0):
-        if self._nq==2:
-            for pair in [('XI','XZ'),('IX','ZX'),('YI','YZ'),('IY','ZY'),('XX','YY')]:
-                temp = self.expectations[pair[0]]
-                self.expectations[pair[0]] = self.expectations[pair[1]]
-                self.expectations[pair[1]] = temp
-    def cx(self,c,t):
-        self.h(t)
-        self.cx(c,t)
-        self.h(t)
-def execute(qc,shots=1024):
-    probs = {}
+  def __init__(qc,nq):
+    qc._nq=nq
+    qc.p2s=['I','X','Y','Z']*(qc._nq==2)+['']*(qc._nq==1)
+    qc.exp={}
+    qc.reset()
+  def _pl(qc,p1,p2,q):
+    return (p1+p2)*(q==1) + (p2+p1)*(q==0)
+  def reset (qc):
+    for p1 in ['I','X','Y','Z']:
+      for p2 in qc.p2s:
+        qc.exp[p1+p2]=(('X' not in (p1+p2))and('Y' not in (p1+p2) ))
+  def x(qc,q):
+    for p2 in qc.p2s:
+      for p1 in ['Y','Z']:
+        qc.exp[qc._pl(p1,p2,q)]=-qc.exp[qc._pl(p1,p2,q)]   
+  def h(qc,q):
+    for p2 in qc.p2s:
+      temp=qc.exp[qc._pl('X',p2,q)]
+      qc.exp[qc._pl('X',p2,q)]=qc.exp[qc._pl('Z',p2,q)]
+      qc.exp[qc._pl('Z',p2,q)]=temp
+      qc.exp[qc._pl('Y',p2,q)]=-qc.exp[qc._pl('Y',p2,q)]
+  def rx(qc,theta,q):
+    c=cos(theta)
+    s=sin(theta)
+    for p2 in qc.p2s:
+      old_z=qc.exp[qc._pl('Z',p2,q)]
+      old_y=qc.exp[qc._pl('Y',p2,q)]
+      qc.exp[qc._pl('Z',p2,q)]=c*old_z-s*old_y
+      qc.exp[qc._pl('Y',p2,q)]=c*old_y+s*old_z
+  def cx(qc,c=0,t=0):
     if qc._nq==2:
-        probs['00'] = (1+qc.expectations['IZ']+qc.expectations['ZI']+qc.expectations['ZZ'])/4
-        probs['01'] = (1-qc.expectations['IZ']+qc.expectations['ZI']-qc.expectations['ZZ'])/4
-        probs['10'] = (1+qc.expectations['IZ']-qc.expectations['ZI']+qc.expectations['ZZ'])/4
-        probs['11'] = (1-qc.expectations['IZ']-qc.expectations['ZI']-qc.expectations['ZZ'])/4
-    elif qc._nq==1:
-        probs['0'] = (1+qc.expectations['Z'])/2
-        probs['1'] = (1-qc.expectations['Z'])/2
-    counts = {}
-    if shots==0:
-        for string in probs:
-            counts[string] = probs[string]
-    else:
-        for string in probs:
-            counts[string] = 0
-        for shot in range(shots):
-            cumu = 0
-            unchosen = True
-            r = random.random()
-            for string in counts:
-                cumu += probs[string]
-                if r<cumu and unchosen:
-                    counts[string] += 1
-                    unchosen = False
-    return counts
+      qc.h(t)
+      for pair in [('XI','XZ'),('IX','ZX'),('YI','YZ'),('IY','ZY'),('XX','YY')]:
+        temp=qc.exp[pair[0]]
+        qc.exp[pair[0]]=qc.exp[pair[1]]
+        qc.exp[pair[1]]=temp
+      qc.h(t)
+def execute(qc,shots=1024):
+  ps={}
+  def s(out):
+    return (1-2*(out=='1'))
+  if qc._nq==2:
+    for out in ['00','01','10','11']:
+      ps[out]=(1+s(out[1])*qc.exp['IZ']+s(out[0])*qc.exp['ZI']+s(out[0])*s(out[1])*qc.exp['ZZ'])/4
+  elif qc._nq==1:
+    for out in ['0','1']:
+      ps[out]=(1+s(out)*qc.exp['Z'])/2
+  c={}
+  if shots==0:
+    for out in ps:
+      c[out]=ps[out]
+  else:
+    for out in ps:
+      c[out]=0
+    for _ in range(shots):
+      cumu=0
+      un=True
+      r=random.random()
+      for out in c:
+        cumu += ps[out]
+        if r<cumu and un:
+          c[out] += 1
+          un=False
+  return c
