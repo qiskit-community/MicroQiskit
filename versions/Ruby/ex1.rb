96fdf0015474f4a9c838384d031b42bc51f0e44e
spencerking <spencerking@users.noreply.github.com>
2020-09-14 17:46:17 -0500

    Add third example, clean up the other two

--- versions/Ruby/ex1.rb
+++ versions/Ruby/ex1.rb
@@ -1,13 +1,13 @@
 load 'MicroQiskit.rb'
 
-psiMinus = QuantumCircuit.new(2, 2);
-psiMinus.h(0);
-psiMinus.x(1);
-psiMinus.cx(0, 1);
-psiMinus.z(1);
-psiMinus.measure(0, 0);
-psiMinus.measure(1, 1);
-psiMinusStatevector = simulate(psiMinus, 0, 'statevector');
+psiMinus = QuantumCircuit.new(2, 2)
+psiMinus.h(0)
+psiMinus.x(1)
+psiMinus.cx(0, 1)
+psiMinus.z(1)
+psiMinus.measure(0, 0)
+psiMinus.measure(1, 1)
+psiMinusStatevector = simulate(psiMinus, 0, 'statevector')
 puts 'psiMinusStatevector: '
 puts psiMinusStatevector
 puts simulate(psiMinus, 5, 'counts')
